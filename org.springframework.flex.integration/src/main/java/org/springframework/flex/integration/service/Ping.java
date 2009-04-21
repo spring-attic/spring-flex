dd47097c1af18c3def543b9ebded6fad368eaabc
Jeremy Grelle <jgrelle@vmware.com>
2009-04-21 20:32:57 +0000

    Modified integration tests to verify FLEX-36 as invalid.

---
+++ org.springframework.flex.integration/src/main/java/org/springframework/flex/integration/service/Ping.java
@@ -0,0 +1,11 @@
+package org.springframework.flex.integration.service;
+
+public interface Ping {
+
+	public String ping();
+
+	public String foo();
+
+	public void fireEvent();
+
+}
--- org.springframework.flex.integration/src/main/java/org/springframework/flex/integration/service/PingService.java
+++ org.springframework.flex.integration/src/main/java/org/springframework/flex/integration/service/PingService.java
@@ -1,22 +1,32 @@
 package org.springframework.flex.integration.service;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.flex.messaging.MessageTemplate;
 
-public class PingService {
+public class PingService implements InitializingBean, Ping {
 
 	private MessageTemplate template;
 	
 	private TaskExecutor taskExecutor;
 	
+	/* (non-Javadoc)
+	 * @see org.springframework.flex.integration.service.Ping#ping()
+	 */
 	public String ping() {
 		return "pong";
 	}
 	
+	/* (non-Javadoc)
+	 * @see org.springframework.flex.integration.service.Ping#foo()
+	 */
 	public String foo() {
 		return "bar";
 	}
 	
+	/* (non-Javadoc)
+	 * @see org.springframework.flex.integration.service.Ping#fireEvent()
+	 */
 	public void fireEvent() {
 		taskExecutor.execute(new EventPublisher());
 	}
@@ -34,6 +44,10 @@ public class PingService {
 
 	public void setTaskExecutor(TaskExecutor taskExecutor) {
 		this.taskExecutor = taskExecutor;
+	}
+
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("_____________________INITIALIZING PING SERVICE______________________");
 	}
 	
 }
