cad4ed1426e6d259ac8725e8350e3190991910b8
Jeremy Grelle <jgrelle@vmware.com>
2010-02-05 23:36:34 +0000

    FLEX-105 - Provide an optional Spring 3 + Spring Security 3 profile to the Maven build for the Test Drive

--- spring-flex-samples/spring-flex-testdrive/testdrive/src/main/java/org/springframework/flex/samples/secured/Security3Helper.java
+++ spring-flex-samples/spring-flex-testdrive/testdrive/src/main/java/org/springframework/flex/samples/secured/Security3Helper.java
@@ -2,9 +2,10 @@ package org.springframework.flex.samples.secured;
 
 import java.util.Map;
 
-import org.springframework.flex.security.AuthenticationResultUtils;
+import org.springframework.flex.security3.AuthenticationResultUtils;
 
-public class SecurityHelper {
+
+public class Security3Helper {
 
     public Map<String, Object> getAuthentication() {
         return AuthenticationResultUtils.getAuthenticationResult();
