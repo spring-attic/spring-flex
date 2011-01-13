97bbe87efc9b79aab3800140fdc8f3f77d7c680d
Jeremy Grelle <jgrelle@vmware.com>
2011-01-13 19:44:24 +0000

    Cleanup warnings.

--- spring-flex-core/src/test/java/org/springframework/flex/core/io/EmbeddedAddress.java
+++ spring-flex-core/src/test/java/org/springframework/flex/core/io/EmbeddedAddress.java
@@ -4,10 +4,6 @@ package org.springframework.flex.core.io;
 import java.util.Date;
 
 import javax.persistence.Embeddable;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Embeddable
 public class EmbeddedAddress {
