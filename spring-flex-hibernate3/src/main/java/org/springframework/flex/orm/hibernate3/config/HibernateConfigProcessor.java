1ab2954ee8b03ddd2a0d4b204fae1a95ef05bf01
Jose Barragan <josebarragan@apache.org>
2014-07-27 19:31:20 +0200

    to squash

--- spring-flex-hibernate3/src/main/java/org/springframework/flex/orm/hibernate3/config/HibernateConfigProcessor.java
+++ spring-flex-hibernate3/src/main/java/org/springframework/flex/orm/hibernate3/config/HibernateConfigProcessor.java
@@ -34,6 +34,7 @@ import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.core.convert.converter.ConverterRegistry;
 import org.springframework.flex.config.MessageBrokerConfigProcessor;
 import org.springframework.flex.core.io.AbstractAmfConversionServiceConfigProcessor;
+import org.springframework.flex.orm.hibernate3.io.HibernateProxyConverter;
 import org.springframework.flex.orm.hibernate3.io.PersistentCollectionConverterFactory;
 import org.springframework.util.Assert;
 
